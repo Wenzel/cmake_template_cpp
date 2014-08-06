f5c4ff6d15be2391398ae258010f826fd28c80cd
Mathieu Tarral <mathieu.tarral@gmail.com>
2014-08-07 00:40:44 +0200

    move doxygen from doc/ to build/doc

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -4,14 +4,14 @@
 find_package(Doxygen)
 if(DOXYGEN_FOUND)
     configure_file(
-        "${CMAKE_SOURCE_DIR}/doc/Doxyfile.in.cmake"
-        "${CMAKE_SOURCE_DIR}/doc/Doxyfile"
+        "${CMAKE_SOURCE_DIR}/cmake/in/Doxyfile.in.cmake"
+        "${CMAKE_BINARY_DIR}/doc/Doxyfile"
         @ONLY
     )
     add_custom_target(
-        doc         # NAME
-        COMMAND doxygen ${CMAKE_SOURCE_DIR}/doc/Doxyfile
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        doc
+        COMMAND doxygen ${CMAKE_BINARY_DIR}/doc/Doxyfile
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
         COMMENT Doxygen is generating the documentation...
         VERBATIM
     )
