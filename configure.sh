10212467aaa36bfdd7337b17fdb954be22cafddc
Mathieu Tarral <mathieu.tarral@gmail.com>
2014-08-07 04:25:03 +0200

    improve project configuration

--- configure.sh
+++ configure.sh
@@ -3,28 +3,27 @@
 # absolute path to current dir
 cur_dir=`dirname \`readlink -f $0\``
 
-# remove old build
-rm -rf "$cur_dir/_build"
-
 # create build dir
 mkdir -p "$cur_dir/_build"
 
-# compilers
+# COMPILERS
+##################
 compilers=(g++ clang++)
-
 dialog --backtitle "Compiler selection" \
     --radiolist "Choose your compiler" 0 0 10 \
-    1 "${compilers[0]}" on\
-    2 "${compilers[1]}" off\
+    1 "${compilers[0]}" on \
+    2 "${compilers[1]}" off \
     2> /tmp/$$_dialog.ans
 
-answer=$(cat /tmp/$$_dialog.ans)
-if [ "$answer" -eq 0 ]; then
-    echo "Cancelled by user"
+compiler_answer=$(cat /tmp/$$_dialog.ans && rm -f /tmp/$$_dialog.ans)
+if [ "$compiler_answer" = "" ] || [ "$compiler_answer" -eq 0 ]; then
+    echo "Project Configuration Cancelled."
     exit 1
 fi
 
-export CXX="/usr/bin/${compilers[$(($answer - 1 ))]}"
-
-# cmake
-cd "$cur_dir/_build" && cmake .. $*
+# CMAKE
+#################
+cd "$cur_dir/_build" && \
+    cmake .. \
+    -DCMAKE_CXX_COMPILER="/usr/bin/${compilers[$(($compiler_answer - 1 ))]}" \
+    $* # rest
