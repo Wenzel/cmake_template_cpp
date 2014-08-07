1b743fc606fd3ee2be76ce4418799a731658beed
Mathieu Tarral <mathieu.tarral@gmail.com>
2014-08-07 03:36:30 +0200

    remove -r option, add dialog box to choose compiler

--- configure.sh
+++ configure.sh
@@ -3,14 +3,28 @@
 # absolute path to current dir
 cur_dir=`dirname \`readlink -f $0\``
 
-if [ "$1" = "-r" ]; then
-    # remove previous build
-    rm -rf "$cur_dir/_build"
-    shift
-fi
+# remove old build
+rm -rf "$cur_dir/_build"
 
 # create build dir
 mkdir -p "$cur_dir/_build"
+
+# compilers
+compilers=(g++ clang++)
+
+dialog --backtitle "Compiler selection" \
+    --radiolist "Choose your compiler" 0 0 10 \
+    1 "${compilers[0]}" on\
+    2 "${compilers[1]}" off\
+    2> /tmp/$$_dialog.ans
+
+answer=$(cat /tmp/$$_dialog.ans)
+if [ "$answer" -eq 0 ]; then
+    echo "Cancelled by user"
+    exit 1
+fi
+
+export CXX="/usr/bin/${compilers[$(($answer - 1 ))]}"
 
 # cmake
 cd "$cur_dir/_build" && cmake .. $*
